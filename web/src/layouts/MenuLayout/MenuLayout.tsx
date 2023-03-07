4101152a558341cbcacb3b9100cc74e3528e51a3
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-03-07 08:53:31 +0100

    Active class name

--- web/src/layouts/MenuLayout/MenuLayout.tsx
+++ web/src/layouts/MenuLayout/MenuLayout.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import useSession from '../../hooks/session.hook';
 
 import Icons from '../../components/Icons';
 
 import { MenuLayoutProps } from './MenuLayout.interface';
+
+const MenuItem = ({ to, icon, label }: any) => {
+	const location = useLocation();
+
+	return (
+		<Link to={to} className={`flex px-1 items-center rounded-3xl ${to === location.pathname ? 'text-white bg-gray-600 stroke-white' : 'text-gray-600 stroke-gray-600 hover:text-white hover:bg-gray-300 hover:stroke-white'} w-52 h-12 text-left`}>
+			<div className='h-9 w-9 rounded-full overflow-hidden mx-3 stroke-1'>
+				<Icons type={icon} />
+			</div>
+			<span>{label}</span>
+		</Link>
+	);
+}
 
 export default function MenuLayout({ menu }: MenuLayoutProps) {
 	const { session } = useSession();
@@ -24,27 +37,12 @@ export default function MenuLayout({ menu }: MenuLayoutProps) {
 							{
 								session.account_configured &&
 								<>
-									<Link to={`/${session.username}`} className='flex px-1 items-center rounded-3xl text-gray-600 stroke-gray-600 w-52 h-12 text-left hover:text-white hover:bg-gray-600 hover:stroke-white'>
-										<div className='h-9 w-9 rounded-full overflow-hidden mx-3 stroke-1'>
-											<Icons type='profile' />
-										</div>
-										<span>Profile</span>
-									</Link>
-									<Link to='/videos' className='flex px-1 items-center rounded-3xl text-gray-600 stroke-gray-600 w-52 h-12 text-left hover:text-white hover:bg-gray-600 hover:stroke-white'>
-										<div className='h-9 w-9 rounded-full overflow-hidden mx-3 stroke-1'>
-											<Icons type='video' />
-										</div>
-										<span>Videos</span>
-									</Link>
-									<Link to='/settings' className='flex px-1 items-center rounded-3xl text-gray-600 fill-gray-600 w-52 h-12 text-left hover:text-white hover:bg-gray-600 hover:fill-white'>
-										<div className='h-9 w-9 rounded-full overflow-hidden mx-3'>
-											<Icons type='settings' />
-										</div>
-										<span>Settings</span>
-									</Link>
+									<MenuItem to={`/${session.username}`} icon='profile' label='Profile' />
+									<MenuItem to={`/videos`} icon='video' label='Videos' />
+									<MenuItem to={`/settings`} icon='settings' label='Settings' />
 								</>
 							}
-							<button className='flex px-1 items-center rounded-3xl text-gray-600 stroke-gray-600 text-base w-52 h-12 text-left hover:text-white hover:bg-gray-600 hover:stroke-white'>
+							<button className='flex px-1 items-center rounded-3xl text-gray-600 stroke-gray-600 text-base w-52 h-12 text-left hover:text-white hover:bg-gray-300 hover:stroke-white'>
 								<div className='h-9 w-9 rounded-full overflow-hidden mx-3 stroke-1'>
 									<Icons type='logout' />
 								</div>

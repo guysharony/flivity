51da16692ce6fb98b55de71408379df3e68841f3
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-03-13 07:43:12 +0100

    Fixing videos header

--- web/src/pages/Videos/Videos.header.tsx
+++ web/src/pages/Videos/Videos.header.tsx
@@ -7,7 +7,7 @@ const VideosHeader = ({ videos }: IVideosHeader) => {
 	return (
 		<div className="pb-4">
 			<div className="flex py-6 justify-between">
-				<div>
+				<div className='flex items-center'>
 					<span className="text-2xl font-sans">Videos</span>
 				</div>
 				<div>

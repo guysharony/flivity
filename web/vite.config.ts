588d11285b649b74ca58c13e3720eb2694749203
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-04-23 10:22:27 +0200

    Configuring vite

---
+++ web/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    pluginRewriteAll(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    splitVendorChunkPlugin(),
+  ],
+  build: {
+    outDir: "build",
+  },
+  server: {
+    port: 3000,
+  },
+});

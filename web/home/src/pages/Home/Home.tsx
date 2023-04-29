a13c501c76845ba1fc6e63ac6699c3f3d2c00315
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-04-29 11:07:19 +0200

    Creating home page

--- web/home/src/pages/Home/Home.tsx
+++ web/home/src/pages/Home/Home.tsx
@@ -1,7 +1,21 @@
+import { Button } from "antd";
+
 function Home() {
 	return (
 		<div>
-			<p>This is home page</p>
+			<div className="flex flex-col text-center py-24">
+				<div className="mb-10">
+					<h1 className="text-7xl mb-10">Lorem ipsum dolor sit amet.<br></br>Dolor consectetur.</h1>
+					<span className="text-2xl text-gray-600">Lorem ipsum dolor sit amet, consectetur.</span>
+				</div>
+				<div className="flex gap-2 mb-10 items-center justify-center">
+					<Button size="large" shape="round" className="text-blue-800 border-blue-800">Start now</Button>
+					<Button type="text" size="large" shape="round" className="text-blue-800">Contact us</Button>
+				</div>
+				<div className="w-full h-96 border-dashed border-black border rounded">
+
+				</div>
+			</div>
 		</div>
 	);
 }

b772a02c6df6541553a5bc85f2d293e46397d0dd
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-05-01 10:40:36 +0200

    Adding home page title and subtitle

--- web/home/src/pages/Home/Home.tsx
+++ web/home/src/pages/Home/Home.tsx
@@ -8,8 +8,10 @@ function Home() {
 		<div>
 			<div className="flex flex-col text-center py-24">
 				<div className="mb-10">
-					<h1 className="text-7xl mb-10">Lorem ipsum dolor sit amet.<br></br>Dolor consectetur.</h1>
-					<span className="text-2xl text-gray-600">Lorem ipsum dolor sit amet, consectetur.</span>
+					<div className="w-full max-w-5xl mx-auto">
+						<h1 className="text-7xl mb-10 text-gray-600"><span className="text-gray-900">Improve</span> and <span className="text-gray-900">simplify</span><br></br>social media management</h1>
+						<span className="text-2xl text-gray-600">From small content creators to large businesses - use Flivity to simplify and improve your social media management.</span>
+					</div>
 				</div>
 				<div className="flex gap-2 mb-10 items-center justify-center">
 					<Button type="primary" size="large" shape='round' href={new URL('/signin', import.meta.env.VITE_APPLICATION_URL).href} ghost>Start now</Button>
